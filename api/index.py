2fe187b5e550340987db10e48085bf09ccbc056a
aelahilham <113027307+aelahilham@users.noreply.github.com>
2026-06-08 09:51:33 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 def get_playlist(path):
     # --- MASUKIN LINK HTTP TOOLKIT LO DI BAWAH INI ---
     playlists = [
-        {"url": "http://liveloveyou.my.id/566450a1/ux.html", "group": "LIVE TV NEW"},
-        {"url": "http://liveloveyou.my.id/566450a1/pelme1.html", "group": "LIVE TV"},
-        {"url": "http://liveloveyou.my.id/566450a1/lv.txt", "group": "LIVE EVENT AUTO"},
+        {"url": "http://liveloveyou.my.id/4385eac8/ux.html", "group": "LIVE TV NEW"},
+        {"url": "http://liveloveyou.my.id/4385eac8/pelme1.html", "group": "LIVE TV"},
+        {"url": "http://liveloveyou.my.id/4385eac8/lv.txt", "group": "LIVE EVENT AUTO"},
         {"url": "https://gvision-web.vercel.app/nw/piIdun.html", "group": "PIALA DUNIA 2026"},
         {"url": "http://liveloveyou.my.id/tesr/belum.html", "group": "JADWAL EVENT AUTO"},
-        {"url": "http://liveloveyou.my.id/566450a1/pelme2.html", "group": "LIVE EVENT MANUAL"},
-        {"url": "http://liveloveyou.my.id/566450a1/pelme3.html", "group": "SPORTS"},
-        {"url": "http://liveloveyou.my.id/566450a1/pelme4.html", "group": "TV LUAR NEGERI"},
+        {"url": "http://liveloveyou.my.id/4385eac8/pelme2.html", "group": "LIVE EVENT MANUAL"},
+        {"url": "http://liveloveyou.my.id/4385eac8/pelme3.html", "group": "SPORTS"},
+        {"url": "http://liveloveyou.my.id/4385eac8/pelme4.html", "group": "TV LUAR NEGERI"},
         {"url": "http://gvision-web.vercel.app/dio.txt", "group": "RADIO"}
     ]
     # -------------------------------------------------
